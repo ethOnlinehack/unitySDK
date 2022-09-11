24193b09077521899dd639e577cf491584520bbf
ZiedChekir <ziedchekir1@gmail.com>
2022-09-11 18:43:38 +0100

    Initial commit

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Threading.Tasks;
+using System;
+
+public class SDK  {
+
+    private string host = "http://localhost:";
+    private string port = "3001";
+    UriBuilder objUri = new UriBuilder();
+    
+// objUri.Scheme = "http";
+// objUri.Port = 80;
+// objUri.Host = "www.microsoft.com";
+// objUri.Path = "en-us/default.aspx";
+
+    public static Task<string> SignIn (){
+
+
+
+    }
+    
+}
